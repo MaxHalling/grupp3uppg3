2fa00b4442f5390999d75d1fd4ce0da338dde24d
MaxHalling <hallingmaax@gmail.com>
2023-09-28 10:39:06 +0200

    initial commit

---
+++ src/main/java/com/example/grupp3uppg3/Grupp3uppg3Application.java
@@ -0,0 +1,13 @@
+package com.example.grupp3uppg3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp3uppg3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupp3uppg3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grupp3uppg3/Grupp3uppg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupp3uppg3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupp3uppg3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
